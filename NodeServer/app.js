be111b0e848aa3483e6127ce8720e99cd24fe58d
josh-horsley <joshua.horsley@outlook.com>
2014-09-03 22:14:03 -0700

    restructuring files

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,8 +1,8 @@
 var io = require('socket.io').listen(5000, { log: false }),
     theodos = require('theodos'),
-    ultrasonic = require('./components/ultrasonic.js'),
-    motor = require('./components/motor.js'),
-    explorer = require('./software/explorer.js');
+    ultrasonic = require('components/ultrasonic'),
+    motor = require('components/motor'),
+    explorer = require('software/explorer');
 
 io.sockets.on('connection', function (socket) {
     var robot = theodos.create({
