a7946627fb263bc420ae07689602cbd2d828c9c3
josh-horsley <joshua.horsley@outlook.com>
2014-09-03 21:59:00 -0700

    Restructuring files

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,15 +1,15 @@
 var io = require('socket.io').listen(5000, { log: false }),
-    robot = require('./robot.js'),
-    ultrasonic = require('./ultrasonic.js'),
-    motor = require('./motor.js'),
-    explorer = require('./explorer.js');
+    theodos = require('theodos'),
+    ultrasonic = require('./components/ultrasonic.js'),
+    motor = require('./components/motor.js'),
+    explorer = require('./software/explorer.js');
 
 io.sockets.on('connection', function (socket) {
-    var hardware = robot.create({
+    var robot = theodos.create({
         'socket': socket,
         'sensor': ultrasonic,
         'motor': motor
     });
 
-    hardware.runEvery(0.1).boot(explorer);
+    robot.runEvery(0.1).boot(explorer);
 });
---
+++ NodeServer/software/explorer.js
@@ -0,0 +1,57 @@
+module.exports = function (components) {
+    var io = components.socket,
+        motor = new components.motor(io),
+        sensor = new components.sensor(io);
+
+    console.log('[[STARTING ROBOT]]');
+    var chosen = false,
+        lastChoice = '',
+        backupThreshold = 0,
+        forwardThreshold = 0;
+
+    this.run = function () {
+        var distance = sensor.GetDistance()
+
+        if (distance == -1 || distance > 10) {
+            if (forwardThreshold > 200) {
+                motor.turn("right");
+            } else {
+                motor.move("forward");
+            }
+
+            lastChoice = "";
+            forwardThreshold++;
+            backupThreshold = 0;
+        } else if (distance < 2) {
+            if (backupThreshold > 5) {
+                motor.turn("right");
+            } else {
+                motor.move("backward");
+            }
+
+            lastChoice = "";
+            backupThreshold++;
+            forwardThreshold = 0;
+            setTimeout(function () {
+                motor.stop();
+            }, 1000);
+        } else if (!chosen) {
+            backupThreshold = 0;
+            forwardThreshold = 0;
+            var choiceDirection = Math.floor((Math.random() * 2) + 1);
+            if (lastChoice != "") {
+                motor.turn(lastChoice);
+            } else if (choiceDirection == 1) {
+                motor.turn("right");
+                lastChoice = "right";
+            } else {
+                motor.turn("left");
+                lastChoice = "left";
+            }
+            chosen = true;
+            setTimeout(function () {
+                chosen = false;
+            }, 2000);
+        }
+    };
+};
